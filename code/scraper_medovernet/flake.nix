5bae4684f39e986092babaf78d05332cdc12bad5
Gašper Spagnolo <gasper.spagnolo@outlook.com>
2024-04-11 18:02:50 +0200

    Scraper done

---
+++ code/scraper_medovernet/flake.nix
@@ -0,0 +1,24 @@
+{
+  description = "Python development environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, ... }@inputs:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [
+            (self: super: {})
+          ];
+        };
+        pythonEnv = pkgs.python3.withPackages
+          (ps: with ps; [ matplotlib numpy seaborn pyyaml selenium requests]);
+      in {
+        devShells.default = pkgs.mkShell { buildInputs = [ pythonEnv ]; };
+      });
+}
+
